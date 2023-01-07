617d51ddd945491b26b141127434553408813816
Rafal <rzi@vp.pl>
2023-01-07 19:27:40 +0100

    works , but without button interactin

--- new_main.py
+++ new_main.py
@@ -1,9 +1,51 @@
+from datetime import datetime
+
+import requests
 from PyQt6.QtWidgets import (
     QApplication, QVBoxLayout, QWidget, QLabel, QPushButton, QTextEdit, QGridLayout
 )
 from PyQt6.QtCore import Qt
-import sys
+import sys,time
 
+Start = 0
+cmd = 'digitemp1.conf'
+cmd1 = './temp'
+timefmt = '%Y-%m-%d %H:%M:%S'
+def odczyt(self,Start):
+    while (Start):
+            print("Start", Start)
+            print(time.strftime('%Y-%m-%d %H:%M:%S'))
+            time.sleep(2)
+            fo = open(cmd1, "r+")
+            Lines = fo.readlines()
+            print(Lines)
+            count = 0
+            for line in Lines:
+                S = str.split(line, " ")
+                # G = S[3]
+                # print(G)
+                # print(S)
+                # count += 1
+                # print("Line{}: {}".format(count, line.strip()))
+                if S[2] == "Sensor":
+                    #     print (S)
+                    print(S[0], S[1], S[3], S[4], S[5], S[6])
+                    my_date = datetime.fromtimestamp(float(S[0])).strftime('%Y-%m-%d %H:%M:%S')
+                    print(my_date)
+                    #     mydate = datetime.datetime.fromtimestamp(float(S[0])).strftime('%Y-%m-%d')
+                    #     # print mydate
+                    mytime = datetime.fromtimestamp(float(S[0])).strftime('%H:%M:%S')
+                    print(mytime)
+                    # print S[0]
+                    my_string = S[0] + " " + S[1] + " " + S[6] + " " + S[4]
+                    print(my_string)
+                    try:
+                        # Execute the SQL command
+                        r = requests.post('https://tempapi.ct8.pl/addtemp',
+                                          json={'my_epoch': S[0], 'nr_hex': S[1], 'temp': S[6], 'nr_czujnika': S[4]})
+                    except:
+                        # Rollback in# case there is any error
+                        print("błąd wysłania do API")
 
 class Window(QWidget):
     def __init__(self):
@@ -63,15 +105,17 @@ class Window(QWidget):
         self.textEdit.setPlainText("")
         layout.addWidget(self.textEdit, 6, 1)
 
-    def start(self):
-        print("Start")
-
-    def stop(self):
-        print("Stop")
-
+        odczyt(self,Start)
+    def start(self, Start):
+        Start = 1
+        print("Start" , Start)
+        odczyt(self, Start)
+    def stop(self, Start):
+        Start = 0
+        print("Stop" , Start)
+        odczyt(self, Start)
     def init(self):
         print("Init")
-
 
 app = QApplication(sys.argv)
 window = Window()
