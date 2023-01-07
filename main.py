ddffbdd770ae16564240c8ae9c27210e3fde8e3f
rafalzietak <rzi@vp.pl>
2023-01-07 15:05:43 +0100

    new window

--- main.py
+++ main.py
@@ -1,56 +1,38 @@
 #!/usr/bin/env python
 from datetime import datetime
 
-from PyQt5.uic.properties import QtWidgets
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel
-from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt
 import sys
+import time
 
-# Press the green button in the gutter to run the script.
+import datetime as datetime
+import requests
+import requests as requests
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+
+
+def display_time():
+    current_time = datetime.datetime.now().strftime('%Y.%m.%d - %H:%M:%S')
+    label.setText(current_time)
+    print('current_time:', current_time)
+
 if __name__ == '__main__':
-    def display_time():
-
-        current_time = datetime.datetime.now().strftime('%Y.%m.%d - %H:%M:%S')
-        label.setText(current_time)
-
-        print('current_time:', current_time)
-    class Window(QWidget):
-        def __init__(self):
-            super().__init__()
-            self.resize(600, 600)
-            self.setWindowTitle("CodersLegacy")
-            self.setWindowIcon(QIcon("icon.jpg"))
-
-            layout = QVBoxLayout()
-            self.setLayout(layout)
-
-            label = QLabel("Hello World")
-            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            layout.addWidget(label)
-            label2 = QLabel("Hello")
-            font = label2.font()
-            font.setPointSize(30)
-            label2.setFont(font)
-            label2.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-            layout.addWidget(label2)
-            # self.setCentralWidget(widget)
-
-    app = QApplication(sys.argv)
-    window = Window()
-    label3 = QLabel('???')
-    window.setCentralWidget(label3)
-    window.show()
-    sys.exit(app.exec())
 
     # cmd = 'sudo digitemp_DS9097U  -a  -c /home/pi/Desktop/BMS/digitemp1.conf'
     # window()
     cmd = 'digitemp1.conf'
     cmd1 = './temp'
     timefmt = '%Y-%m-%d %H:%M:%S'
+    app = QApplication([])
+    window = QWidget()
+    window.setWindowTitle("PyQt App")
+    window.setGeometry(100, 100, 280, 80)
+    helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
+    helloMsg.move(60, 15)
+    window.show()
+    sys.exit(app.exec())
 
     while True:
-        print(label)
+        print("label")
         label = QLabel("START1")
         # print(time.strftime('%Y-%m-%d %H:%M:%S'))
         time.sleep(2)
@@ -67,16 +49,16 @@ if __name__ == '__main__':
             # print("Line{}: {}".format(count, line.strip()))
             if S[2] == "Sensor":
             #     print (S)
-                print (S[0], S[1], S[3], S[4],S[5], S[6])
+                print(S[0], S[1], S[3], S[4], S[5], S[6])
                 my_date = datetime.fromtimestamp(float(S[0])).strftime('%Y-%m-%d %H:%M:%S')
-                print (my_date)
+                print(my_date)
             #     mydate = datetime.datetime.fromtimestamp(float(S[0])).strftime('%Y-%m-%d')
             #     # print mydate
                 mytime = datetime.fromtimestamp(float(S[0])).strftime('%H:%M:%S')
-                print (mytime)
+                print(mytime)
                 # print S[0]
                 my_string = S[0] + " " + S[1] + " " + S[6] + " " + S[4]
-                print (my_string)
+                print(my_string)
                 try:
                     # Execute the SQL command
                     r = requests.post('https://tempapi.ct8.pl/addtemp', json={'my_epoch': S[0], 'nr_hex': S[1],'temp': S[6],'nr_czujnika': S[4]})
@@ -118,4 +100,5 @@ if __name__ == '__main__':
         # disconnect from server
         # db.close() # poza if
         # db2.close()  # poza if
+
 
